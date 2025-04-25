9b150c4f5f97b77353599de733f3260ab1a35f24
Argeo <argeoprosperi22@gmail.com>
2025-04-25 14:03:40 +0200

    refactor study path dto

--- src/main/java/com/school/dto/StudyPathDTO.java
+++ src/main/java/com/school/dto/StudyPathDTO.java
@@ -3,7 +3,6 @@ package com.school.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -12,5 +11,5 @@ public class StudyPathDTO {
     private Long id;
     private String name;
     private String description;
-    private Set<CourseDTO> courses;
+    private Integer duration;
 }
